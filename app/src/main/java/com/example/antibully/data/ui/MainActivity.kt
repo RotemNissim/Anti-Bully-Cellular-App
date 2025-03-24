f626d87fb7f26b9ac4b915a693c1299664eca486
RotemNissim <rotemnissim28@gmail.com>
2025-03-24 15:24:36 +0200

    top app bar - not showing correctly

--- app/src/main/java/com/example/antibully/data/ui/MainActivity.kt
+++ app/src/main/java/com/example/antibully/data/ui/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.antibully.data.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.antibully.R
-import com.example.antibully.data.models.MessageRequest
-import com.example.antibully.data.api.RetrofitClient
-import com.example.antibully.data.firestore.FirestoreManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.google.android.material.appbar.MaterialToolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,13 +19,27 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_menu)
+        val topAppBar = findViewById<MaterialToolbar>(R.id.top_app_bar)
+
 
         bottomNav.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.loginFragment, R.id.signUpFragment -> bottomNav.visibility = View.GONE
-                else -> bottomNav.visibility = View.VISIBLE
+                R.id.loginFragment, R.id.signUpFragment -> {
+                    bottomNav.visibility = View.GONE
+                    topAppBar.visibility = View.GONE
+
+                }
+                else ->{
+                    bottomNav.visibility = View.VISIBLE
+                    topAppBar.visibility = View.VISIBLE
+                    topAppBar.setNavigationIcon(R.drawable.ic_back_arrow) // Replace with your icon
+                    topAppBar.setNavigationOnClickListener {
+                        navController.navigateUp()
+                    }
+                    topAppBar.title = destination.label
+                }
             }
         }
 
